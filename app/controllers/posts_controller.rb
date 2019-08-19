9c77e5bd62c5e5a0ccf9fa9d3802e2644250159b
ayerest <irisiseve@gmail.com>
2019-08-19 11:57:50 -0700

    made urgency level a dropdown - added to posts show page as well

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,7 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new(post_params)
+        @post.user_id = session[:user_id]
         @post.save
         redirect_to post_path(@post)
     end
