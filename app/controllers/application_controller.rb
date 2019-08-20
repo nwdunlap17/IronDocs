9c923e04ac9528e0c88bbb02aab213a4063256ad
ayerest <irisiseve@gmail.com>
2019-08-20 11:00:59 -0700

    merging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     helper_method :check_for_login
 
         def check_for_login
-            if !!session[:user_id]
+            if !!!session[:user_id]
                 redirect_to login_path
             end
         end
