0bceaae0fdc1d9380bbc9bc2f2705aa94915c294
Nick Dunlap <nwdunlap17@gmail.com>
2019-08-20 11:00:22 -0700

    seperate helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
-    helper_method :check_for_login
+    helper_method :check_for_login, :logged_in?
 
         def check_for_login
-            if !!session[:user_id]
+            if !logged_in?
                 redirect_to login_path
             end
+        end
+
+        def logged_in?
+            return !!session[:user_id]
         end
 end
